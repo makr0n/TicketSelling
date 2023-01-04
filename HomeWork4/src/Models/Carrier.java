65d22d1bebd8bed8f750f59b462d8d2d137f0aa4
MashtalirM <mashtalir.m@mail.ru>
2023-01-04 18:14:32 +0300

    Carrier class v0.0.1

--- HomeWork4/src/Models/Carrier.java
+++ HomeWork4/src/Models/Carrier.java
@@ -1,13 +1,27 @@
 package Models;
 
+import java.util.ArrayList;
+
 /**
- * Модель перевозчика
+ * Модель перевозчика v0.0.1
  */
 public class Carrier {
+    private int id;
+    private ArrayList<Integer> zones;
+    private long cardNumber;
 
-    public Carrier(int id, long cardNumber) {
+
+    public Carrier(int id, ArrayList<Integer> zones, long cardNumber) {
         this.id = id;
+        this.zones = zones;
         this.cardNumber = cardNumber;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
 }
